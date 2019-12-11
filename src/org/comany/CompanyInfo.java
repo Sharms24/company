2ec139b9da8005ffb8f9848061a74eea107f0b5c
sharms24 <sharmijaya123@gmail.com>
2019-12-11 16:03:44 +0530

    Company Details

---
+++ src/org/comany/CompanyInfo.java
@@ -0,0 +1,27 @@
+package org.comany;
+
+public class CompanyInfo {
+	public void  companyName() {
+		System.out.println("company name is  tcs");
+	}
+	
+	public void  companyId(){
+		System.out.println("company  id is 5698");
+		
+	}
+	public void  companyAddress() {
+		System.out.println("company address yyyyy");
+			
+	}
+	
+		public  static  void  main(String[] args){
+			
+	CompanyInfo a=new CompanyInfo();
+				a.companyName();
+				
+			  a.companyId();
+				a.companyAddress();
+				
+			}
+			
+}
